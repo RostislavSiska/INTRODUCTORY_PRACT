7fd9e1c34ed1b79b2227dc6a7ec0adac390ae8bd
Rostislick <yaretsro@mail.ru>
2025-07-01 00:38:48 +0700

    Сделаны все функции, кроме web_image

--- main.py
+++ main.py
@@ -1,21 +1,116 @@
 import cv2 as cv
 import numpy as np
 
+#C:/Users/Rostislick/Desktop/bocchi.png
 
-SHOW_IMAGE = 1
-SHOW_RGB = 2
-DOWN_BRIGHT = 3
-SHOW_GREY = 4
-DRAW_RECTANGLE = 5
+def load_image():
+    try:
+        image = cv.imdecode(np.fromfile(input("Введите путь к фото: "), dtype=np.uint8), cv.IMREAD_COLOR)
+    except:
+        print("Некоректный ввод")
+    return image
 
-n = -1
+def web_image():
+    pass
+
+def show_image(image, name_of_window = "Image"):
+    cv.namedWindow(name_of_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(name_of_window, 800, 500)
+    cv.imshow(name_of_window, image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def show_rgb(image):
+    print("1) red\n" \
+      "2) green \n" \
+      "3) blue ")
+    choice = input("Какой канал показать: ")
+    
+    RED = "1"
+    GREEN = "2"
+    BLUE = "3"
+
+    if choice == BLUE:
+        blue = image.copy()
+        # set green and red channels to 0
+        blue[:, :, 1] = 0
+        blue[:, :, 2] = 0
+        show_image(blue)
+    elif choice == GREEN:
+        green = image.copy()
+        # set blue and red channels to 0
+        green[:, :, 0] = 0
+        green[:, :, 2] = 0
+        show_image(green)
+    elif choice == RED:
+        red = image.copy()
+        # set blue and green channels to 0
+        red[:, :, 0] = 0
+        red[:, :, 1] = 0
+        show_image(red)
+    else: print("smth goes wrong")
+
+def down_bright(image):
+    hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv)
+
+    v = cv.add(v, -40)
+
+    hsv_bright = cv.merge([h, s, v])
+    brightened = cv.cvtColor(hsv_bright, cv.COLOR_HSV2BGR)
+    show_image(brightened)
+
+def show_grey(image):
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    show_image(gray_image)
+
+def draw_rectangle(image, name_of_window = "Image"):
+    a, b = map(int, input("Верхний левый угол [x1, y1]: ").split())
+    start_point = (a, b)
+    c, d = map(int, input("Нижний-правый угол [y1, y2]: ").split())
+    end_point = (c, d)
+    color  = (255, 0, 0)
+    thickness = -1
+    cv.rectangle(image, start_point, end_point, color, thickness)
+    show_image(image)
+
+
+EXIT = "0"
+LOAD_IMAGE = "1"
+WEB_IMAGE = "2"
+SHOW_IMAGE = "3"
+SHOW_RGB = "4"
+DOWN_BRIGHT = "5"
+SHOW_GREY = "6"
+DRAW_RECTANGLE = "7"
 
 while True:
     print ("Выберите операцию:", "\n",
            "0. Выход", "\n",
-           "1. Показать изображение", '\n',
-           "2. Показать RGB", "\n",
-           "3. Понизить яркость", "\n",
-           "4. Показать изображение в оттенках серого", "\n",
-           "5. Показать прямоугольник на изображении")
+           "1. Загрузить картинку", "\n",
+           "2. Картинка с вебкамеры", "\n",
+           "3. Показать изображение", '\n',
+           "4. Показать RGB", "\n",
+           "5. Понизить яркость", "\n",
+           "6. Показать изображение в оттенках серого", "\n",
+           "7. Нарисовать прямоугольник на изображении")
     n = input("Введите номер операции: ")
+    if n == EXIT:
+        exit()
+        print("Пока.")
+    elif n == LOAD_IMAGE:
+        image = load_image()
+    elif n == WEB_IMAGE:
+        web_image()
+    elif n == SHOW_IMAGE:
+        show_image(image)
+    elif n == SHOW_RGB:
+        show_rgb(image)
+    elif n == DOWN_BRIGHT:
+        down_bright(image)
+    elif n == SHOW_GREY:
+        show_grey(image)
+    elif n == DRAW_RECTANGLE:
+        draw_rectangle(image)
+    else:
+        print("Некоректный ввод")
