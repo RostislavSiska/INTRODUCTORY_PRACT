671e4af09766ae539501430d623fb56659d7850c
Rostislick <yaretsro@mail.ru>
2025-07-01 16:54:19 +0700

    Все функции переделаны под tkinter

--- Photo.py
+++ Photo.py
@@ -86,14 +86,14 @@ class PhotoEditor:
         btn_red = Button(rgb_window, text="RED", command=self.show_red)
         btn_red.pack()
 
-        btn_green = Button(rgb_window, text="GREEn", command=self.show_green)
+        btn_green = Button(rgb_window, text="GREEN", command=self.show_green)
         btn_green.pack()
 
 
 
 
     def show_blue(self):
-        image_copy = self.image
+        image_copy = self.image.copy()
         # set green and red channels to 0
         self.image[:, :, 1] = 0
         self.image[:, :, 2] = 0
@@ -102,7 +102,7 @@ class PhotoEditor:
 
 
     def show_red(self):
-        image_copy = self.image
+        image_copy = self.image.copy()
         # set green and red channels to 0
         self.image[:, :, 0] = 0
         self.image[:, :, 1] = 0
@@ -110,7 +110,7 @@ class PhotoEditor:
         self.image = image_copy
 
     def show_green(self):
-        image_copy = self.image
+        image_copy = self.image.copy()
         # set green and red channels to 0
         self.image[:, :, 0] = 0
         self.image[:, :, 2] = 0
@@ -118,6 +118,19 @@ class PhotoEditor:
         self.image = image_copy
 
     def down_bright(self):
+
+        def close():
+            try:
+                beta_n = entry.get()
+                beta = int(beta_n) * (-1)
+                self.image = cv.convertScaleAbs(self.image, alpha=1, beta=beta)
+                self.show_image()
+                downbrig_window.destroy()
+                return beta
+            except:
+                # Окно с ошибкой
+                print("Некоректный ввод")
+                return 0
 
         downbrig_window = Tk()
         downbrig_window.title("Down bright")
@@ -127,23 +140,68 @@ class PhotoEditor:
 
         entry = Entry(downbrig_window, width=30)
         entry.pack(pady=5, padx=10)
-        try:
-                entry.bind()
-                beta = entry.get()*(-1)
-                image_copy = self.image
-                self.image = cv.convertScaleAbs(self.image, alpha=1, beta=beta)
-                entry.delete(0, END)
-                downbrig_window.destroy()
-                self.show_image()
-                self.image = image_copy
-        except:
-            print("Некоректный ввод")
+        image_copy = self.image.copy()
+        
+        btn_close = Button(downbrig_window, text="Принять", command=close)
+        btn_close.pack(pady=15, padx=10)
+
+        self.image = image_copy
+
+        downbrig_window.mainloop()
         
     def show_grey(self):
-        image_copy = self.image
+        image_copy = self.image.copy()
         self.image = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         self.show_image()
         self.image = image_copy
+
+    def draw_rectangle(self):
+
+        def close():
+            try:
+                x1_n = entry_x1.get()
+                x1 = int(x1_n)
+                y1_n = entry_y1.get()
+                y1 = int(y1_n)
+                x2_n = entry_x2.get()
+                x2 = int(x2_n)
+                y2_n = entry_y2.get()
+                y2 = int(y2_n)
+                cv.rectangle(self.image, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=-1)
+                self.show_image()
+                draw_window.destroy()
+            except:
+                # Окно с ошибкой
+                print ("Некоректный ввод")
+        
+        draw_window = Tk()
+        draw_window.title("Draw rectangle")
+        draw_window.geometry("300x300")
+
+        Label(draw_window, text="Верхний-левый угол прямоугольника: ").pack()
+        Label(draw_window, text="X1").pack()
+        entry_x1 = Entry(draw_window, width=5)
+        entry_x1.pack()
+        Label(draw_window, text="Y1").pack()
+        entry_y1 = Entry(draw_window, width=5)
+        entry_y1.pack()
+        Label(draw_window, text="Нижний-правый угол прямоугольника: ").pack()
+        Label(draw_window, text="X1").pack()
+        entry_x2 = Entry(draw_window, width=5)
+        entry_x2.pack()
+        Label(draw_window, text="Y1").pack()
+        entry_y2 = Entry(draw_window, width=5)
+        entry_y2.pack()
+
+        image_copy = self.image.copy()
+
+
+        btn_close = Button(draw_window, text="Принять", command=close)
+        btn_close.pack(pady=15, padx=10)
+
+        self.image = image_copy
+
+        draw_window.mainloop()
 
     def functional_menu(self):
         
@@ -151,22 +209,22 @@ class PhotoEditor:
         functional_window.title("Functional window")
         functional_window.geometry("300x300")
 
-        btn_show_image = Button(functional_window, text="Show image", command=self.show_image)
+        btn_show_image = Button(functional_window, text="Показать фото", command=self.show_image)
         btn_show_image.pack()
 
-        btn_show_rgb = Button(functional_window, text="Show RGB image", command=self.rgb_menu)
+        btn_show_rgb = Button(functional_window, text="Показать каналы RGB", command=self.rgb_menu)
         btn_show_rgb.pack()
 
     
-        btn_down_bright = Button(functional_window, text="Down bright", command=self.down_bright)
+        btn_down_bright = Button(functional_window, text="Понизить яркость", command=self.down_bright)
         btn_down_bright.pack()
     
-        btn_show_grey = Button(functional_window, text="Show grey image", command=self.show_grey)
+        btn_show_grey = Button(functional_window, text="Показать изображение в оттенках серого", command=self.show_grey)
         btn_show_grey.pack()
-    """
-        btn_draw_rectangle = Button(functional_window, text="Draw rectangle", command=self.draw_rectangle)
-        btn_draw_rectangle.pack()
     
+        btn_draw_rectangle = Button(functional_window, text="Нарисовать прямоугольник на фото", command=self.draw_rectangle)
+        btn_draw_rectangle.pack()
+    """
         btn_back = Button(functional_window, text="Back", command=self.back_start_menu)
         btn_back.pack()
     """
